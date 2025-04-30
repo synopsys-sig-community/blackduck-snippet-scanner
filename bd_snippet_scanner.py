56c9585525d740b8b3ba533063265c2dfe683757
Jouni Lehto <lehto.jouni@gmail.com>
2025-04-30 15:47:15 +0300

    fixed java cp

--- bd_snippet_scanner.py
+++ bd_snippet_scanner.py
@@ -39,8 +39,7 @@ class SnippetScanner:
             self.gitcommenter = GihubCommenter(gittoken=gittoken, giturl=giturl, repo=repo, prID=prID, changedFiles=changedOnly, group=group, log_level=log_level)
 
     def __hashFileContent(self, file) -> str:
-        p = subprocess.Popen(f"java -cp \"../blackduck-snippet-scanner/snippet-scanner-1.0-SNAPSHOT.jar;../blackduck-snippet-scanner/sca-fingerprint-client-1.0.0.jar\" com.blackduck.snippet.App \"{file}\"", stdout=subprocess.PIPE, shell=True)
-        # p = subprocess.Popen(f"java -cp \"snippet-scanner-1.0-SNAPSHOT.jar;sca-fingerprint-client-1.0.0.jar\" com.blackduck.snippet.App \"{file}\"", stdout=subprocess.PIPE, shell=True)
+        p = subprocess.Popen(f"java -cp \"snippet-scanner-1.0-SNAPSHOT.jar;sca-fingerprint-client-1.0.0.jar\" com.blackduck.snippet.App \"{file}\"", stdout=subprocess.PIPE, shell=True)
         output, err = p.communicate()
         if err:
             logging.error(err)
