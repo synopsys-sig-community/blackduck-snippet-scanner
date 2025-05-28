6a6a6359ed07fbeba53e5bc005bd3d449ec7019a
Jouni Lehto <lehto.jouni@gmail.com>
2025-05-28 11:16:59 +0300

    conclusion set to action_required

--- snippetGithubCommenter.py
+++ snippetGithubCommenter.py
@@ -237,4 +237,4 @@ class GihubCommenter:
             "text": summaryText
         }
         self.repo.create_check_run(name="Snippet Analysis Results", head_sha=self.pullRequest.head.sha, status="completed", 
-                                   conclusion="Conclusion", output=summary)
+                                   conclusion="action_required", output=summary)
